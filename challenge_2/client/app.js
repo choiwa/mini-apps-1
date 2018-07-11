f4153cdc42e7ef986eb2be8de0b530f3cab3090d
Joyce Cho <joyjoycho@gmail.com>
2018-07-10 18:45:58 -0700

    server post working properly

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,16 @@
+var url = "http://127.0.0.1:3000/"
 
-
+$(document).ready(() => {
+  $("button").click((e)=> {
+    e.preventDefault();
+    //console.log($("#jsonString").val())
+    $.ajax({
+      url: url,
+      type: 'POST',
+      contentType: 'text/plain',
+      data: $("#jsonString").val(),
+      success: function (data) {
+      }
+    })
+  })
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,29 @@
+// define route and resources
+var express = require('express');
+var app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.text());
+// app.use(express.static('client'))
+// app.use will look for the index.html and render
+app.use('/', express.static(path.join(__dirname, 'client')));
+
+app.post('/', function (req, res) {
+  var data = req.body;
+  var parsedData = JSON.parse(data);
+  for (var key in parsedData) {
+    console.log(key);
+  }
 
 
+
+
+  // res.send();
+})
+
+app.listen(3000)
+
+// require Express
+// invoke Express
+// app.use to find the folder to render
+// app.listen for starting location of the server
